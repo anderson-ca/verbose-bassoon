7b6881682305883f9a37f1f5fabf76bbf15c82f6
anderson <anderson.d.cardoso92@gmail.com>
2022-02-16 11:42:47 -0600

    use grid for website layout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,12 +9,19 @@ import About from '../components/About';
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Navbar />
-      <Hero />
-      <About />
+      <ul className="my-grid">
+        <li className="grid-item">item 1</li>
+        <li className="grid-item">item 2</li>
+        <li className="grid-item">item 3</li>
+        <li className="grid-item">item 4</li>
+        <li className="grid-item">item 5</li>
+      </ul>
+      {/* <Navbar /> */}
+      {/* <Hero /> */}
+      {/* <About /> */}
       <Component {...pageProps} />
       {/* <WavesDivider /> */}
-      <Footer />
+      {/* <Footer /> */}
     </Layout>
   )
 }
---
+++ pages/grids.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Grids.module.css';
+
+const grids = () => {
+  return (
+    <div className={styles.grid_container}>
+        grids
+    </div>
+  )
+}
+
+export default grids
